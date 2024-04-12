379cab05d91bac4791e330d5e6620f1c0dbbd0e7
Shubham Juneja <shubhamjuneja@Shubhams-MacBook-Air.local>
2024-04-13 03:12:39 +0530

    Added scrapy worldometer code

---
+++ webscraping/scrapy/my_first_scrapy/my_first_scrapy/spiders/worldometer.py
@@ -0,0 +1,15 @@
+import scrapy
+
+
+class WorldometerSpider(scrapy.Spider):
+    name = 'worldometer'
+    allowed_domains = ['www.worldometers.info/']
+    start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
+
+    def parse(self, response):
+        title = response.xpath('//h1/text()').get()
+        countries = response.xpath('//td/a/text()').getall()
+        yield {
+            'title':title,
+            'country': countries
+        }
